b01281add3b161b86aaa071fcd7f04ea91bd9e50
acui145 <acui145@gmail.com>
2021-11-10 11:26:54 +0800

    提取配置

--- cuijian/ime.lua
+++ cuijian/ime.lua
@@ -1,6 +1,7 @@
 --------------------------------------------------------------------
 ---------------------- 输入法自动切换及提醒 ---------------------------
 --------------------------------------------------------------------
+custom_config = hs.fs.pathToAbsolute(os.getenv("HOME") .. '/.config/hammerspoon/private/config.lua')
 
 local inputEnglish = "com.apple.keylayout.ABC"
 local inputChinese = "com.apple.inputmethod.SCIM.ITABC"
@@ -15,7 +16,7 @@ local function English()
 end
 
 -- app to expected ime config
-local app2Ime = {
+local app2Ime = hsime_config or {
   {'/Applications/iTerm.app', 'English'},
   {'/Applications/Visual Studio Code.app', 'English'},
   {'/Applications/Notion.app', 'Chinese'},
@@ -34,6 +35,7 @@ local app2Ime = {
   {'/Applications/wechatwebdevtools.app', 'English'},
   -- {'/Applications/Sketch.app', 'English'},
 }
+local showChangeAlert = hsime_change
 
 function updateFocusAppInputMethod()
   local focusAppPath = hs.window.frontmostWindow():application():path()
@@ -69,28 +71,30 @@ function applicationWatcher(appName, eventType, appObject)
 end
 
 function showMsgAtTopRight(msg)
+  hs.alert.closeAll()
   hs.alert.show(msg, {
     atScreenEdge = 2
   })
 end
 
 -- 监听输入法切换
-local lastInputSourceID = hs.keycodes.currentSourceID();
-function inputSourceChangeWatcher()
-  local currentInputSource = hs.keycodes.currentSourceID();
-  if (lastInputSourceID ~= currentInputSource) then
-    lastInputSourceID = currentInputSource
-    if (currentInputSource == inputEnglish) then
-      showMsgAtTopRight('英文输入');
-    end
+if (showChangeAlert) then
+  local lastInputSourceID = hs.keycodes.currentSourceID();
+  function inputSourceChangeWatcher()
+    local currentInputSource = hs.keycodes.currentSourceID();
+    if (lastInputSourceID ~= currentInputSource) then
+      lastInputSourceID = currentInputSource
+      if (currentInputSource == inputEnglish) then
+        showMsgAtTopRight('英文输入');
+      end
 
-    if (currentInputSource == inputChinese) then
-      showMsgAtTopRight('中文输入');
+      if (currentInputSource == inputChinese) then
+        showMsgAtTopRight('中文输入');
+      end
     end
   end
+  hs.keycodes.inputSourceChanged(inputSourceChangeWatcher)
 end
-
-hs.keycodes.inputSourceChanged(inputSourceChangeWatcher)
 
 appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
--- private/config.lua
+++ private/config.lua
@@ -109,6 +109,30 @@ hsreload_keys = {{"cmd", "shift", "ctrl"}, "R"}
 -- 显示各种模式绑定快捷键
 hshelp_keys = {{"alt", "shift"}, "/"}
 
+----------------------------------------------------------------------------------------------------
+--------------------------------- 输入法自动切换设置 --------------------------------------------------
+-- app设置
+hsime_config = {
+    {'/Applications/iTerm.app', 'English'},
+    {'/Applications/Visual Studio Code.app', 'English'},
+    {'/Applications/Notion.app', 'Chinese'},
+    -- {'/Applications/Xcode.app', 'English'},
+    {'/Applications/Google Chrome.app', 'English'},
+    {'/System/Library/CoreServices/Finder.app', 'English'},
+    -- {'/Applications/DingTalk.app', 'Chinese'},
+    -- {'/Applications/Kindle.app', 'English'},
+    {'/Applications/NeteaseMusic.app', 'Chinese'},
+    {'/Applications/WeChat.app', 'Chinese'},
+    {'/Applications/企业微信.app', 'Chinese'},
+    {'/Applications/System Preferences.app', 'English'},
+    {'/Applications/Dash.app', 'English'},
+    {'/Applications/MindNode.app', 'Chinese'},
+    {'/Applications/Preview.app', 'Chinese'},
+    {'/Applications/wechatwebdevtools.app', 'English'},
+    -- {'/Applications/Sketch.app', 'English'},
+}
+-- 显示输入法切换
+hsime_change = true
 
 ----------------------------------------------------------------------------------------------------
 ---------------------------------------------- end  ------------------------------------------------
